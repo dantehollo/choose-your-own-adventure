1f3b63c7dd02fef9f171a8403bb2c8ba63c3ea90
Matthew Caravagio <matthew.caravaggio@protonmail.com>
2020-11-07 01:04:36 -0500

    fixed a depreciation warning

--- code/mern/models/connection.js
+++ code/mern/models/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectionString = process.env.MONGODB_URI || "mongodb://localhost/db-chooseYourOwnAdventure";
 
-mongoose.connect(connectionString, { useNewUrlParser: true})
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("connected to mongo at: " + connectionString);
   });
