f88e022d12111c7d54a0189494de9392d88b28cd
Matthew Caravagio <matthew.caravaggio@protonmail.com>
2020-11-06 19:20:51 -0500

    Trying to figure out a scalable way to inject the script into the game

---
+++ code/js/character.js
@@ -0,0 +1,7 @@
+export default class Character {
+    constructor(name, portrait, emotion){
+          this.name = name
+          this.portrait = portrait
+          this.emotion = emotion
+    }
+}
